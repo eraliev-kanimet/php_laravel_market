56773a510167ebfe2d3393b42b7fe01ae62596eb
kani <eraliev-kanimet>
2022-05-07 19:27:43 +0600

    Created a method for loading an image

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,8 +6,24 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Intervention\Image\Facades\Image;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * @param $file
+     * @return string
+     */
+    public function saveImage($file): string
+    {
+        $dir = 'storage/pictures/' . date('Y') . '/' . date('m') . '/' . date('d');
+        if (!file_exists($dir)) {
+            mkdir($dir, 0775, true);
+        }
+        $path = $dir . '/' . md5(time()) . $file->getClientOriginalName();
+        Image::make($file)->resize(600, 500)->save($path);
+        return $path;
+    }
 }
